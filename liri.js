867c0066e5f41109f64d71b3028e497bc7623bc5
TSchaeken-Deskop <TSchaeken@gmail.com>
2018-04-18 00:09:21 -0700

    added all functions

--- liri.js
+++ liri.js
@@ -1,10 +1,51 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const request = require('request');
-const Twitter = require('twitter')
-const Spotify = require('node-spotify-api');
-const keys = require('./keys.js')
+//node packages
+
+const request = require("request");
+const Twitter = require("twitter");
+const Spotify = require("node-spotify-api");
+const inquirer = require("inquirer");
+
+//extra .js files and .env generated keys
+
+const keys = require("./keys.js");
+const twitterSearch = require("./twitter-search");
+const spotifySearch = require("./spotify-search");
+const omdbSearch = require("./omdb-search");
+const special = require("./special");
+
+//attaching keys to requests
 
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
+//inquirer function for CLI
+
+function Start() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Choose an option.",
+        choices: [
+          "my tweets",
+          "spotify this song",
+          "movie this",
+          "do what it says"
+        ]
+      }
+    ])
+    .then(function(res) {
+      if (res.choice === "my tweets") {
+      } else if (res.choice === "spotify this song") {
+      } else if (res.choice === "movie this") {
+      } else if (res.choice === "do what it says") {
+      } else {
+        console.log("error");
+      }
+    });
+}
+
+Start();
---
+++ twitter-search.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+
+const Twitter = require('twitter')
+
+const client = new Twitter(keys.twitter);
